035f292fa68d088654241d53f2d362d743443818
Roberto Russo <rob.russo@reply.it>
2022-10-21 13:14:29 +0200

    feat: add build_env.ps1

--- status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Memory available"
-grep MemTotal /proc/meminfo | awk '{print $2}' | xargs -I {} echo "scale=4; {}/1024^2" | bc
-echo "CPUs available"
-lscpu | grep -E '^Thread|^Core|^Socket|^CPU\('
-
