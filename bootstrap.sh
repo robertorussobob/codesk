1aef044228167d5a9c14102f737e04b2f4e9924b
Roberto Russo <robertorusso.bob@gmail.com>
2022-12-09 10:35:17 +0000

    fix: nvm

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,6 +49,8 @@ tmux
 
 # nvm and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 node --versionnewgrp docker
 npm --version
