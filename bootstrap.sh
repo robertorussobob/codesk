eaf9901ef92ac304c902fbcc9b35733403179411
Roberto Russo <robertorusso.bob@gmail.com>
2022-12-25 11:08:20 +0100

    feat: add gcloud CLI

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,6 +47,11 @@ pylint \
 ranger \
 tmux
 
+# gcloud CLI
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-cli
+
 # Docker
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo mkdir -p /etc/apt/keyrings
