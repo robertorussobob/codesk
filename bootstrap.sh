273cac6c936d9f1a741a968167f9353a6d72b57b
Roberto Russo <robertorusso.bob@gmail.com>
2023-04-13 19:24:33 +0200

    feat!: install Python 3.9 (AWS) side by side to Ubuntu 22 LTS 3.10

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
+APT_OPT="-q=2"
+
 # Python repo
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get update -q=2 && sudo apt-get upgrade -q=2
-sudo apt-get install software-properties-common -q=2
+sudo apt-get update $APT_OPT && sudo apt-get upgrade $APT_OPT
+sudo apt-get install software-properties-common $APT_OPT
 
-sudo apt-get update -q -y
+sudo apt-get update $APT_OPT
+
+sudo apt-get install $APT_OPT \
+python3.9 \
+python3.9-dev \
+python3.9-distutils
+
 sudo update-alternatives --remove python /usr/bin/python2
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 10
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11 11
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 10
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 11
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 11
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 11
 
 # pip
 curl -sS https://bootstrap.pypa.io/get-pip.py | python
 #export PATH="$PATH:~/.local/bin"
 
 # AWS cli
-sudo apt-get install -q=2 unzip
+sudo apt-get install $APT_OPT unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
 sudo ./aws/install
@@ -32,7 +40,7 @@ sudo ./sam-installation/install
 rm -rf sam-installation
 sam --version
 
-sudo apt-get install -q=2 \
+sudo apt-get install $APT_OPT \
 uidmap \
 apt-transport-https \
 ca-certificates \
@@ -53,7 +61,6 @@ tmux
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo --user=vagrant sh ./get-docker.sh
 dockerd-rootless-setuptool.sh install
-#export PATH=/usr/bin:$PATH
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 cat >> ~/.profile <<EOF
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
