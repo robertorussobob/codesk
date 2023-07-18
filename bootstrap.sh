7c14d63a05a66859bc1e85270bda19a2a335cbd4
Roberto Russo <robertorusso.bob@gmail.com>
2023-07-18 22:51:33 +0200

    feat: install minikube

--- bootstrap.sh
+++ bootstrap.sh
@@ -126,4 +126,9 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
 sudo apt update -y
 sudo apt-get install -y terraform
 
+# minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm -f minikube-linux-amd64
+
 sudo lvextend /dev/ubuntu-vg/ubuntu-lv -L+31G -r
