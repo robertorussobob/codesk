f29fbb30abce1c0ae156712f4a58b4524bee9eb5
Roberto Russo <robertorusso.bob@gmail.com>
2025-06-07 10:07:07 +0200

    sudo apt purge nodejs npm

--- bootstrap.sh
+++ bootstrap.sh
@@ -108,6 +108,7 @@ git-remote-codecommit \
 yq
 
 # nvm, npm, yarn
+sudo apt purge nodejs npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
